a4cc01ad67cb6d7e5090a7efc7415ed68033eadd
Your Name <eochair@aol.com>
2021-01-01 21:12:43 -0500

    update

--- 16-D3/Instructions/StarterCode/assets/js/app.js
+++ 16-D3/Instructions/StarterCode/assets/js/app.js
@@ -25,4 +25,51 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
   var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
+// Import Data
+d3.csv("data.csv").then(function(povData) {
+  //  Parse the data
+  
+  povData.forEach(function(data){
+   data.poverty=+data.poverty;
+   data.healthcare=+data.healthcare;
+  });
+  })
+//Step 5: Create Scales
+
+var xLinearScale=d3.scalelinear()
+.domain([8, d3.max(povData, d=>d.poverty)])
+.range([0, width]);
+
+var yLinearScale=d3.scalelinear()
+.domain([8, d3.max(povData, d=>d.healthcare)])
+.range([height, 0]);
+
+//Create Axes
+var bottomAxis = d3.axisBottom(xLinearScale);
+var leftAxis = d3.axisLef(yLinearScale);
+// Append the axes to the chartGroup
+chartGroup.append("g")
+.attr("transform", `translate(0, ${height})`)
+.call(bottomAxis);
+
+chartGroup.append("g")
+.call(leftAxis);
+
+
+// Create Circles
+var circlesGroup = chartGroup.selectAll("circle")
+.data(povData)
+.enter()
+.append("circle")
+.attr("cx", d => xLinearScale(d.poverty))
+.attr("cy", d => yLinearScale(d.healthcare))
+.attr("r", "15")
+.attr("fill", "blue")
+.attr("opacity", ".5");
+
+// Add Labels to the Circles
+
+// Add Axis Labels
+
+
 
