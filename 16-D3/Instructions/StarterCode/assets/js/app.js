935c0b7dbfb9af82a04e23c724ca135d846eb06c
Your Name <eochair@aol.com>
2021-01-06 15:31:43 -0500

    update

--- 16-D3/Instructions/StarterCode/assets/js/app.js
+++ 16-D3/Instructions/StarterCode/assets/js/app.js
@@ -13,9 +13,8 @@ var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart,
-// and shift the latter by left and top margins.
-var svg = d3
-  .select("#scatter")
+// and shift the latter by left and top margins.Ref HW LESSON d3; 3/12
+var svg = d3.select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
@@ -23,30 +22,29 @@ var svg = d3
 // Append an SVG group
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
-  var chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
-// Import Data
-d3.csv("data.csv").then(function(povData) {
-  //  Parse the data
+ 
+// Import Data; ref 
+d3.csv("data.csv").then(function(povertyData) {
+
   
-  povData.forEach(function(data){
-   data.poverty=+data.poverty;
-   data.healthcare=+data.healthcare;
+  povertyData.forEach(function(data) {
+   data.poverty = +data.poverty;
+   data.healthcare = +data.healthcare;
   });
-  })
+  
 //Step 5: Create Scales
 
-var xLinearScale=d3.scalelinear()
-.domain([8, d3.max(povData, d=>d.poverty)])
+var xLinearScale = d3.scaleLinear()
+.domain([8, d3.max(povertyData, d => d.poverty)])
 .range([0, width]);
 
-var yLinearScale=d3.scalelinear()
-.domain([8, d3.max(povData, d=>d.healthcare)])
+var yLinearScale = d3.scaleLinear()
+.domain([0, d3.max(povertyData, d => d.healthcare)])
 .range([height, 0]);
 
 //Create Axes
 var bottomAxis = d3.axisBottom(xLinearScale);
-var leftAxis = d3.axisLef(yLinearScale);
+var leftAxis = d3.axisLeft(yLinearScale);
 // Append the axes to the chartGroup
 chartGroup.append("g")
 .attr("transform", `translate(0, ${height})`)
@@ -56,9 +54,9 @@ chartGroup.append("g")
 .call(leftAxis);
 
 
-// Create Circles
+// Create Circles. Ref https://www.w3schools.com/graphics/svg_circle.asp
 var circlesGroup = chartGroup.selectAll("circle")
-.data(povData)
+.data(povertyData)
 .enter()
 .append("circle")
 .attr("cx", d => xLinearScale(d.poverty))
@@ -67,9 +65,46 @@ var circlesGroup = chartGroup.selectAll("circle")
 .attr("fill", "blue")
 .attr("opacity", ".5");
 
-// Add Labels to the Circles
+// Append Labels list to the Circles. ref https://www.w3schools.com/graphics/svg_text.asp
+//https://www.w3schools.com/jsref/met_node_appendchild.asp
+//https://stackoverflow.com/questions/21988698/create-position-and-add-text-in-circle-using-d3-using-g-and-svg-in-javascrip
+var circle = svg.selectAll("circlesGroup")
+ .data(povertyData)
+ .enter()
+// Add Axis with state "abbr" Labels
+circle.append("text")
+.attr("x", function(d) { return xLinearScale(d.poverty); })
+.attr("y", function(d) { return yLinearScale(d.healthcare); })
+.text(function(d) { return d.abbr})
+.attr("font-size", "12px")
+.attr("dx", "7.6em")
+.attr("dy", "2em")
+.style("fill", "white")
+.style("font-weight", "bold");
 
-// Add Axis Labels
+//Adding y Axis Labels 
+chartGroup.append("text")
+.attr("transform", "rotate(-90)")
+.attr("y", 0 - margin.left + 40)
+.attr("x", 0 - (height / 2))
+.attr("dy", "1em")
+.attr("class", "axisText")
+.text("Population Healthcare (%)")
+.style("fill", "black");
+
+//Adding x Axis Labels 
+chartGroup.append("text")
+.attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+.attr("class", "axisText")
+.text("Population Poverty (%)")
+.style("fill", "black");
+
+     
+  }).catch(function(error) {
+    console.log(error);
+  });
+
+
 
 
 
